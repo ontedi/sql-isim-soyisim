44b293fafe16ec48e5b3bd6ba9cc2c6871b409d4
ontedi <universityofcu@gmail.com>
2025-09-26 17:38:02 +0300

    Update rastgele-kisiler-olusturalim.sql

--- rastgele-kisiler-olusturalim.sql
+++ rastgele-kisiler-olusturalim.sql
@@ -3,5 +3,33 @@ id INT PRIMARY KEY NOT NULL IDENTITY(1,1),
 isim VARCHAR(100),
 soyisim VARCHAR(100),
 sehirid INT,
-ilceid INT
+ilceid INT,
+dogumtarihi DATE
 )
+
+
+
+DECLARE @IsimSayisi INT
+DECLARE @SoyisimSayisi INT
+DECLARE @IlceSayisi INT
+DECLARE @Rastgele AS INT
+DECLARE @DogumTarihi AS DATE
+
+SELECT @IsimSayisi = COUNT(*) FROM Isimler
+
+SELECT @SoyisimSayisi = COUNT(*) FROM Soyisimler
+
+SELECT @IlceSayisi = COUNT(*) FROM Ilceler
+
+SET @Rastgele = RAND() * @IsimSayisi
+SELECT * FROM Isimler WHERE id = @Rastgele
+
+SET @Rastgele = RAND() * @SoyisimSayisi
+SELECT * FROM Soyisimler WHERE id = @Rastgele
+
+SET @Rastgele = RAND() * @IlceSayisi
+SELECT * FROM Ilceler WHERE id = @Rastgele
+
+SET @Rastgele = RAND() * 365 * 80
+SET @DogumTarihi = GETDATE() - @Rastgele
+SELECT @DogumTarihi
