43593e92117bdb285b7b3a592d9b970a7d7eea09
ontedi <universityofcu@gmail.com>
2025-10-01 14:18:36 +0300

    Update rastgele-kisiler-olusturalim.sql

--- rastgele-kisiler-olusturalim.sql
+++ rastgele-kisiler-olusturalim.sql
@@ -11,34 +11,34 @@ dogumtarihi DATE
 DECLARE @SAYAC INT = 0
 WHILE @SAYAC < 1000000
 BEGIN
-	DECLARE @Isim VARCHAR(100)
-	DECLARE @IsimSayisi INT
-	DECLARE @Soyisim VARCHAR(100)
-	DECLARE @SoyisimSayisi INT
-	DECLARE @IlceSayisi INT
-	DECLARE @SehirId INT
-	DECLARE @IlceId INT
-	DECLARE @Rastgele AS INT
-	DECLARE @DogumTarihi AS DATE
+   DECLARE @Isim VARCHAR(100)
+   DECLARE @IsimSayisi INT
+   DECLARE @Soyisim VARCHAR(100)
+   DECLARE @SoyisimSayisi INT
+   DECLARE @IlceSayisi INT
+   DECLARE @SehirId INT
+   DECLARE @IlceId INT
+   DECLARE @Rastgele AS INT
+   DECLARE @DogumTarihi AS DATE
 
-	SELECT @IsimSayisi = COUNT(*) FROM Isimler
+   SELECT @IsimSayisi = COUNT(*) FROM Isimler
 
-	SELECT @SoyisimSayisi = COUNT(*) FROM Soyisimler
+   SELECT @SoyisimSayisi = COUNT(*) FROM Soyisimler
 
-	SELECT @IlceSayisi = COUNT(*) FROM Ilceler
+   SELECT @IlceSayisi = COUNT(*) FROM Ilceler
 
-	SET @Rastgele = RAND() * @IsimSayisi
-	SELECT @Isim = isim FROM Isimler WHERE id = @Rastgele
+   SET @Rastgele = RAND() * @IsimSayisi
+   SELECT @Isim = isim FROM Isimler WHERE id = @Rastgele
 
-	SET @Rastgele = RAND() * @SoyisimSayisi
-	SELECT @Soyisim = soyisim FROM Soyisimler WHERE id = @Rastgele
+   SET @Rastgele = RAND() * @SoyisimSayisi
+   SELECT @Soyisim = soyisim FROM Soyisimler WHERE id = @Rastgele
 
-	SET @Rastgele = RAND() * @IlceSayisi
-	SELECT @SehirId = sehirid, @IlceId = id FROM Ilceler WHERE id = @Rastgele
+   SET @Rastgele = RAND() * @IlceSayisi
+   SELECT @SehirId = sehirid, @IlceId = id FROM Ilceler WHERE id = @Rastgele
 
-	SET @Rastgele = RAND() * 365 * 80
-	SET @DogumTarihi = GETDATE() - @Rastgele
+   SET @Rastgele = RAND() * 365 * 80
+   SET @DogumTarihi = GETDATE() - @Rastgele
 
-	INSERT INTO Kisiler (isim, soyisim, sehirid, ilceid, dogumtarihi) VALUES (@Isim, @Soyisim, @SehirId, @IlceId, @DogumTarihi)
-	SET @SAYAC = @SAYAC + 1
+   INSERT INTO Kisiler (isim, soyisim, sehirid, ilceid, dogumtarihi) VALUES (@Isim, @Soyisim, @SehirId, @IlceId, @DogumTarihi)
+   SET @SAYAC = @SAYAC + 1
 END
